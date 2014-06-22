005190f4a6526cecb8d795e0ddbd9c45889f7e07
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-21 23:43:55 -0700

    Extracted contents of iso and set up filesystem

--- execute.sh
+++ execute.sh
@@ -4,5 +4,16 @@ echo Installing required tools
 apt-get install -y aptitude
 aptitude install squashfs-tools genisoimage
 
-echo Create
+echo Setting up working directory
 mkdir ~/livecdtmp
+mv ubuntu-11.04-desktop-amd64.iso ~/livecdtmp/
+cd ~/livecdtmp
+
+echo Extracting contents of image
+mkdir mnt
+mount -o loop ubuntu-9.04-desktop-i386.iso mnt
+mkdir extract-cd
+rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract-cd
+
+unsquashfs mnt/casper/filesystem.squashfs
+mv squashfs-root edit
