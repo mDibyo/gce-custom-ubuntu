c2d7c3e87bbdc16e713fc3aecd2954c2d62095b7
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-21 23:52:38 -0700

    Mounted and chrooted

--- execute.sh
+++ execute.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo Installing required tools
+echo "Installing required tools"
 apt-get install -y aptitude
 aptitude install squashfs-tools genisoimage
 
-echo Setting up working directory
+echo "Setting up working directory"
 mkdir ~/livecdtmp
 mv ubuntu-11.04-desktop-amd64.iso ~/livecdtmp/
 cd ~/livecdtmp
 
-echo Extracting contents of image
+echo "Extracting contents of image"
 mkdir mnt
 mount -o loop ubuntu-9.04-desktop-i386.iso mnt
 mkdir extract-cd
@@ -17,3 +17,15 @@ rsync --exclude=/casper/filesystem.squashfs -a mnt/ extract-cd
 
 unsquashfs mnt/casper/filesystem.squashfs
 mv squashfs-root edit
+
+echo "Preparing for chroot"
+cp /run/resolvconf/resolv.conf edit/etc/
+cp /etc/hosts edit/etc/
+
+mount --bind /dev/ edit/dev
+chroot edit
+
+echo "Entered chroot and mounting required directories"
+mount -t proc none /proc
+mount -t sysfs none /sys
+mount -t devpts none /dev/pts
