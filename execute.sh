f1bb96d64ed2caf4f9029548af08fc45721620fe
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-22 00:04:43 -0700

    Removed non-important packages

--- execute.sh
+++ execute.sh
@@ -29,3 +29,14 @@ echo "Entered chroot and mounting required directories"
 mount -t proc none /proc
 mount -t sysfs none /sys
 mount -t devpts none /dev/pts
+export HOME=/root
+export LC_ALL=C
+
+echo "Customizing build to fit requirements"
+dbus-uuidgen > /var/lib/dbus/machine-id
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+
+apt-get remove --purge -y libreoffice*
+apt-get clean
+apt-get autoremove
