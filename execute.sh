4667555bcfa6135f0966eefa13ad84bf94db3c38
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2014-06-23 00:01:41 -0700

    Produced image

--- execute.sh
+++ execute.sh
@@ -117,3 +117,22 @@ umount /sys
 umount /dev/pts
 exit
 umount edit/dev
+
+echo "Producing image"
+chmod +w extract-cd/casper/filesystem.manifest
+chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
+cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
+sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
+sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
+
+rm extract-cd/casper/filesystem.squashfs
+mksquashfs edit extract-cd/casper/filesystem.squashfs # -nolzma
+
+printf $(sudo du -sx --block-size=1 edit | cut -f1) > extract-cd/casper/filesystem.size
+sed -i 's/^#define DISKNAME  Ubuntu 14.04 LTS "Trusty Tahr" - Release amd64/#define DISKNAME  Ubuntu 14.04 LTS "Trusty Tahr" - Release amd64 EDIT/' extract-cd/README.diskdefines
+
+cd extract-cd
+rm md5sum.txt
+find -type f -print0 | xargs -0 md5sum | grep -v isolinux/boot.cat | tee md5sum.txt
+
+mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ../ubuntu-14.04.1-desktop-amd64-custom.iso .
